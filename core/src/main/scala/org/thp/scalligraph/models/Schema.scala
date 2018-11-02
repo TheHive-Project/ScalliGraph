c8f6c46219df219699af44010be5b7ea2d74155b
To-om <toom@thehive-project.org>
2018-11-02 20:28:42 +0100

    Refactor schema definition

--- core/src/main/scala/org/thp/scalligraph/models/Schema.scala
+++ core/src/main/scala/org/thp/scalligraph/models/Schema.scala
@@ -1,9 +1,16 @@
 package org.thp.scalligraph.models
+
 import javax.inject.{Inject, Named, Singleton}
 import org.thp.scalligraph.auth.UserSrv
 import play.api.Logger
 
-trait Schema
+case class InitialValue[V <: Product](model: Model.Vertex[V], value: V)
+
+trait Schema {
+  def modelList: Seq[Model]
+  def initialValues: Seq[InitialValue[_]]
+  def getModel(label: String): Option[Model] = modelList.find(_.label == label)
+}
 
 @Singleton
 class SchemaChecker @Inject()(@Named("schemaVersion") version: Int, schema: Schema, db: Database, userSrv: UserSrv) {
