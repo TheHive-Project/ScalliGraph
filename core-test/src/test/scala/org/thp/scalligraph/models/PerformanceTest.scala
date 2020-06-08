eded790fdec17294c8bd0075c82eef8485a004fe
To-om <toom@thehive-project.org>
2020-06-08 08:48:17 +0200

    Improve performance tests

--- core-test/src/test/scala/org/thp/scalligraph/models/PerformanceTest.scala
+++ core-test/src/test/scala/org/thp/scalligraph/models/PerformanceTest.scala
@@ -1,10 +1,13 @@
 package org.thp.scalligraph.models
 
+import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
 import gremlin.scala._
 import org.specs2.specification.core.Fragments
 import play.api.test.PlaySpecification
 import org.thp.scalligraph.{RichSeq, VertexEntity}
 import org.thp.scalligraph.auth.{AuthContext, AuthContextImpl}
+import org.thp.scalligraph.janus.JanusDatabase
 import play.api.{Configuration, Environment}
 import play.api.libs.logback.LogbackLoggerConfigurator
 
@@ -25,7 +28,7 @@ class TimingStats {
     val sum    = iteration.sum
     val mean   = sum.toDouble / count
     val stddev = Math.sqrt(iteration.foldLeft(0d)((acc, i) => acc + Math.pow(i.toDouble - mean, 2)) / count)
-    s"Timing: $count iterations, mean: ${mean}ms, stddev: $stddev"
+    s"Timing: $count iterations, mean: ${mean}ms, stddev: $stddev, min: ${iteration.min}ms, max: ${iteration.max}ms"
   }
 }
 
@@ -38,68 +41,110 @@ class PerformanceTest extends PlaySpecification {
   val authContext: AuthContext = AuthContextImpl("me", "", "", "", Set.empty)
   sequential
 
-  Fragments.foreach(new DatabaseProviders().list) { dbProvider =>
-    "unique index" in {
-      implicit val db: Database                     = dbProvider.get()
-      val model: Model.Vertex[EntityWithUniqueName] = Model.vertex[EntityWithUniqueName]
+  Fragments.foreach(
+    new DatabaseProviders(
+      Configuration(ConfigFactory.parseString("""
+                                                |db {
+                                                |  onConflict {
+                                                |    maxAttempts = 6
+                                                |    minBackoff = 100 milliseconds
+                                                |    maxBackoff = 1 seconds
+                                                |    randomFactor = 0.2
+                                                |  }
+                                                |  chunkSize = 32k
+                                                |  janusgraph {
+                                                |    storage {
+                                                |      backend: cql
+                                                |      hostname: ["172.17.0.2"]
+                                                |      cql {
+                                                |        cluster-name: Test Cluster
+                                                |        keyspace: thehive
+                                                |      }
+                                                |    }
+                                                |    ids {
+                                                |      block-size: 500000
+                                                |    }
+                                                |  }
+                                                |}
+                                                |""".stripMargin)),
+//      num-partitions: 10
+//        renew-percentage: 0.3
+//      |    cache {
+//        |      db-cache: true
+//        |    }
+      //      |      db-cache-clean-wait: 1000
+      ActorSystem("DatabaseProviders")
+    ).list
+  ) { dbProvider =>
+//    "unique index" in {
+//      implicit val db: Database                     = dbProvider.get()
+//      val model: Model.Vertex[EntityWithUniqueName] = Model.vertex[EntityWithUniqueName]
+//
+//      def getOrCreate(entity: EntityWithUniqueName)(implicit graph: Graph): EntityWithUniqueName with Entity =
+//        db.labelFilter("EntityWithUniqueName")(graph.V)
+//          .has(Key("name") -> entity.name)
+//          .headOption()
+//          .fold(db.createVertex(graph, authContext, model, entity))(vertex => model.toDomain(vertex))
+//
+//      db.createSchema(model)
+//      val createTiming = new TimingStats
+//      (1 to 100000).toTry { i =>
+//        if (i % 1000 == 0) println(s"creation: $i")
+//        createTiming.time {
+//          db.tryTransaction { implicit graph =>
+//            Try(getOrCreate(EntityWithUniqueName(i.toString, i)))
+//          }
+//        }
+//      }
+//      val getTiming = new TimingStats
+//      (1 to 100000).toTry { i =>
+//        if (i % 1000 == 0) println(s"retrieve: $i")
+//        getTiming.time {
+//          db.tryTransaction { implicit graph =>
+//            Success(getOrCreate(EntityWithUniqueName(i.toString, i + 1)).value must_== i)
+//          }
+//        }
+//      }
+//      println(s"[unique index] creation: $createTiming")
+//      println(s"[unique index] retrieve: $getTiming")
+//      ok
+//    }
 
-      def getOrCreate(entity: EntityWithUniqueName)(implicit graph: Graph): EntityWithUniqueName with Entity =
-        db.labelFilter("EntityWithUniqueName")(graph.V)
+    "basic index" in {
+      val iterations                          = 10000
+      val initDb: Database                    = dbProvider.get()
+      val model: Model.Vertex[EntityWithName] = Model.vertex[EntityWithName]
+      initDb.createSchema(model).get
+      val db = initDb match {
+        case j: JanusDatabase => j.batchMode
+        case other            => other
+      }
+
+      def getOrCreate(entity: EntityWithName)(implicit graph: Graph): EntityWithName with Entity =
+        db.labelFilter("EntityWithName")(graph.V)
           .has(Key("name") -> entity.name)
           .headOption()
           .fold(db.createVertex(graph, authContext, model, entity))(vertex => model.toDomain(vertex))
 
       db.createSchema(model)
       val createTiming = new TimingStats
-      (1 to 1000).toTry { i =>
-        createTiming.time {
-          db.tryTransaction { implicit graph =>
-            Try(getOrCreate(EntityWithUniqueName(i.toString, i)))
-          }
-        }
-      }
-      val getTiming = new TimingStats
-      (1 to 1000).toTry { i =>
-        getTiming.time {
-          db.tryTransaction { implicit graph =>
-            Success(getOrCreate(EntityWithUniqueName(i.toString, i + 1)).value must_== i)
-          }
-        }
-      }
-      println(s"[unique index] creation: $createTiming")
-      println(s"[unique index] retrieve: $getTiming")
-      ok
-    }
-
-    "basic index" in {
-      implicit val db: Database               = dbProvider.get()
-      val model: Model.Vertex[EntityWithName] = Model.vertex[EntityWithName]
-
-      def getOrCreate(entity: EntityWithName)(implicit graph: Graph): EntityWithName with Entity =
-//        db.labelFilter("EntityWithName")(graph.V)
-//          .has(Key("name") -> entity.name)
-//          .headOption()
-//          .fold(
-        db.createVertex(graph, authContext, model, entity)
-//          )(vertex => model.toDomain(vertex))
-
-      db.createSchema(model)
-      val createTiming = new TimingStats
-      (1 to 1000).toTry { i =>
+      (1 to iterations).toTry { i =>
+        if (i % 1000 == 0) println(s"creation: $i")
         createTiming.time {
           db.tryTransaction { implicit graph =>
             Try(getOrCreate(EntityWithName(i.toString, i)))
           }
         }
-      }
+      }.get
       val getTiming = new TimingStats
-      (990 to 1100).toTry { i =>
+      (iterations * 9 / 10 + 1 to iterations * 11 / 10).toTry { i =>
+        if (i % 1000 == 0) println(s"retrieve: $i")
         getTiming.time {
           db.tryTransaction { implicit graph =>
             Success(getOrCreate(EntityWithName(i.toString, i + 1)).value)
           }
         }
-      }
+      }.get
       val findDuplicatedTiming = new TimingStats
       findDuplicatedTiming.time {
         db.tryTransaction { implicit graph =>
